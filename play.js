6c564e2095436b5b4586b947b52540de864d59bd
cvetkov-a <sashobgb@abv.bg>
2016-10-13 01:08:38 +0300

    AmmoMan

--- play.js
+++ play.js
@@ -3,19 +3,27 @@ window.onload = function(){function init(){
     let image = document.getElementById('wall')
     let soldier2 = document.getElementById('soldier');
     let wallBack = document.getElementById('wallBack');
+    let ammoMan = document.getElementById('healAmmo');
+    let bomb2 = document.getElementById('bomb');
+    let bomb = {x:20, y:344}
     let soldier = {x:20, y:344}
+    let ammoManObj = {x:20, y:-210}
     let upOrDown = true;
+    let bombsDetonated = 0;
     let br = 0;
     let bullets = 10;
-    let osamasGetAway = 0;
+    let soldiersGetAway = 0;
     let gotAway = true;
     let shotsSucceeded=0;
     let speed = 1;
+    let ammoManCount = 0;
+    let bombExists = false;
+    let bombCounter = 0;
     window.addEventListener('click', result)
     window.addEventListener('click', ammoResult)
     window.addEventListener('click', bombResult)
+    let ammoManOn = false;
     animatedSoldiers();
-    draw()
     function result(){
         if(
             (event.clientX<soldier.x+38
@@ -75,15 +83,28 @@ window.onload = function(){function init(){
         }
         bullets-=1/2;
     }
+    }
     function animatedSoldiers(){
         ctx.clearRect(soldier.x,260,soldier2.width,84)
         ctx.drawImage(wallBack,0,0,800,600,0,0,800,600)
         ctx.drawImage(soldier2,soldier.x,soldier.y)
-	if(bombExists){
-	    ctx.drawImage(bomb2,bomb.x,bomb.y)
-	    if(bomb.y>100)
-            bomb.y-=5;
-	}
+        if(ammoManOn == true){
+            ctx.drawImage(ammoMan, ammoManObj.x, ammoManObj.y);
+            ammoManObj.y+=5;
+            if(ammoManObj.y>440){
+                ammoManObj.y = -210;
+                ammoManObj.x=-75;
+                ammoManObj.x += 80 * (Math.floor(Math.random() * 10) + 1);
+                //ctx.clearRect(ammoManObj.x, 260, ammoMan.width, ammoMan.height);
+                ammoManOn=false;
+                ammoManCount++;
+            }
+        }
+        if((shotsSucceeded-1)%5==0) if(ammoManCount!=0)ammoManCount--;
+        if(shotsSucceeded%5==0 && shotsSucceeded !=0) {
+            if(ammoManCount==0)
+            {ammoManOn = true;}
+        }
 	if(shotsSucceeded>2 && shotsSucceeded%3==0){
             if(bombExists==false){
 	    bomb.x=soldier.x+15;
@@ -105,7 +126,7 @@ window.onload = function(){function init(){
             }
             else if(soldier.y>344){
                 upOrDown=true;br++;
-                if(gotAway==true)osamasGetAway++;
+                if(gotAway==true)soldiersGetAway++;
                 gotAway=true;
             }
             soldier.y +=speed;
@@ -117,18 +138,19 @@ window.onload = function(){function init(){
 	}
 	ctx.fillStyle='white';
         ctx.font = "20px arial black"
-        ctx.fillText(`Osamas caught: ${shotsSucceeded}`,10,500)
+        ctx.fillText(`Soldiers caught: ${shotsSucceeded}`,10,500)
         ctx.fillText(`Bullets left: ${bullets}`,10,530)
-        ctx.fillText(`Osamas who got away: ${osamasGetAway}`,10,560)
-        if(bullets>0 && osamasGetAway<6)
+        ctx.fillText(`Soldiers who got away: ${soldiersGetAway}`,10,560)
+        ctx.font="30px arial black"
+        if(bullets>0 && soldiersGetAway<6)
         requestAnimationFrame(animatedSoldiers);
         else{
             ctx.clearRect(0,0,800,600);
             ctx.fillStyle = 'black'
             ctx.fillRect(0,0,800,600);
             ctx.fillStyle = 'white'
-            if(bullets>0 && osamasGetAway==6)
-                ctx.fillText(`You lost. Lots of enemies went through your deffence.`,120,300)
+            if(bullets>0 && soldiersGetAway==6)
+                ctx.fillText(`You lost. Lots of enemies went through your defence.`,120,300)
             else
                 ctx.fillText(`You lost. You went out of bullets and could resist the attack.`,75,300)
             ctx.fillText(`Enemies caught: ${shotsSucceeded}`,292,330)
